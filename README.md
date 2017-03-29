689199d64f19384e1e4988b72016f61b3ae17580
benoit <benoit.delville@gmail.com>
2017-03-30 01:35:55 +1300

    Initial

---
+++ README.md
@@ -0,0 +1,12 @@
+#### Date-Range picker adaptative
+
+// todo insert gif when released
+
+Quickly select a date span with a slider. The slider precision smoothly change with the date depending of the range selection mode.
+
+
+## How to integrate in your project
+
+
+## How to use
+
---
+++ app/src/main/java/eu/hithredin/easingdate/EDatePickerView.kt
@@ -0,0 +1,16 @@
+package eu.hithredin.easingdate
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+/**
+ * INSERT DOC
+ */
+class EDatePickerView : RelativeLayout {
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+}
