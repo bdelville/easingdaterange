21821cf7f3fbac9c810e4adcef37d8594aa1953e
benoit <benoit.delville@gmail.com>
2017-04-09 23:27:16 +1200

    TODO List

--- app/src/main/java/eu/hithredin/easingdate/EDatePickerView.kt
+++ app/src/main/java/eu/hithredin/easingdate/EDatePickerView.kt
@@ -2,6 +2,7 @@ package eu.hithredin.easingdate
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.RelativeLayout
 
 /**
@@ -13,4 +14,18 @@ class EDatePickerView : RelativeLayout {
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+
+    fun init(attrs: AttributeSet) {
+        LayoutInflater.from(context).inflate(R.layout.date_picker, this, true)
+    }
+
+    // TODO Extract Range slider picker from
+    // TODO https://github.com/twotoasters/MaterialRangeSlider/blob/master/app/src/main/java/com/ticketmaster/mobilestudio/materialrangeslider/MaterialRangeSlider.java
+
+    // TODO Display meaningful date (date / date range / date hours according to precision)
+
+    // TODO Make range configurable, or even better dynamic (if range small the slider itself expands in a nice animation
+    // TODO the width of the bar could represent the precision
+
+    //TODO Then just unit tests and ease of use integration
 }
